14a34efaf0066fb1e3a5256e7348cb98d1970738
Carpten <372907906@qq.com>
2017-08-02 15:53:37 +0800

    Initial commit

--- albumlib/src/main/java/com/ysq/album/activity/AlbumPreviewActivity.java
+++ albumlib/src/main/java/com/ysq/album/activity/AlbumPreviewActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.github.chrisbanes.photoview.PhotoView;
